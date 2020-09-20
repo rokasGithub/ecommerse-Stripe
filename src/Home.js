d13d3624d20f3ae86c190016476c54a461fe0e05
rokasGithub <rokasjobmail@gmail.com>
2020-09-19 17:07:19 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2020/RedHood/Launch/GW/US-EN_080320_RedHood_ACQ_GW_Hero_D_1500x600_CV2._CB406776645_.jpg"
+        />
+
+        <div className="home_row"></div>
+
+        <div className="home_row"></div>
+
+        <div className="home_row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ32aXTWIsXSmub0mgy3fUnunH8aczUkI",
+    authDomain: "ecmmorse-example.firebaseapp.com",
+    databaseURL: "https://ecmmorse-example.firebaseio.com",
+    projectId: "ecmmorse-example",
+    storageBucket: "ecmmorse-example.appspot.com",
+    messagingSenderId: "919466189544",
+    appId: "1:919466189544:web:4ead7db03124363d95df5d"
+  };
